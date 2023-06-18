47b1becdb04a39074c26e0c6485722f77642140d
andreitoledo <andreiltoledo@hotmail.com>
2023-06-18 16:59:18 -0300

    Ajustes na solução inicial

--- src/Altsystems.FeedRSSBlogsAnalyticsApi/Program.cs
+++ src/Altsystems.FeedRSSBlogsAnalyticsApi/Program.cs
@@ -6,11 +6,8 @@ namespace Altsystems.FeedRSSBlogsAnalyticsApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                        
+            builder.Services.AddControllers();            
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
